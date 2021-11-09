23be9832434b1d45ce652724db8107c22a8d3088
Etec 244 <61015263+JoaoLCasagrande@users.noreply.github.com>
2021-11-09 12:26:20 -0200

    Update pratos-detail.component.ts

--- src/app/pratos-detail/pratos-detail.component.ts
+++ src/app/pratos-detail/pratos-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Prato } from '../pratos/prato/prato.model';
+import { PratosService } from '../pratos/pratos.service';
 
 @Component({
   selector: 'app-pratos-detail',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PratosDetailComponent implements OnInit {
 
-  constructor() { }
+  pratos: Prato;
+
+  constructor(private pratosService: PratosService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.pratosService.pratosById(this.route.snapshot.params['id'])
+    .subscribe(pratos => {
+      this.pratos - pratos;
+    })
   }
 
 }
