ad04a554cccb339f5099e7057b72bfc1716d2c4e
Etec 244 <61015263+JoaoLCasagrande@users.noreply.github.com>
2021-11-09 12:26:28 -0200

    Update pratos.service.ts

--- src/app/pratos/pratos.service.ts
+++ src/app/pratos/pratos.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { MEAT_API } from '../app.api';
 import { Prato } from './prato/prato.model';
 
 @Injectable({
@@ -9,9 +10,29 @@ import { Prato } from './prato/prato.model';
 export class PratosService {
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private toastr: ToastrService) {}
 
-  pratos(): Observable<Prato[]> {
-    return this.http.get<Prato[]>(`http://localhost:3000/pratos`);
-  }
+    pratos(): Observable<Prato[]>{
+      return this.http.get<Prato[]>(`${MEAT_API}/pratos`).pipe(
+        map(pratos => pratos),
+        catchError(erro => this,exibeErro(erro))
+      );
+    }
+
+    pratoById(id: String): Observable<Prato>{
+      return this.http.get<Prato>(`${MEAT_API}/pratos/${id}`).pipe(
+        map(pratos => pratos),
+        catchError(erro => this,exibeErro(erro))
+      );
+    }
+
+    exibeErro(e: any): Observable<any> {
+      this.exibirMensagem('ERRO!', 'Não foi possível executar essa operação', 'toast-error');
+      return EMPTY
+    }
+
+    exibirMensagem(titulo: string, mensagem: string, tipo: string): void {
+      this.toastr.show(mensagem, titulo, {closeButton:true, progressBar: true}, tipo)
+    }
 }
